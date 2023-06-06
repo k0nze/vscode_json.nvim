284ffdcf3224fc4fddf6f38bf7a1a7f3cb7a2ec9
Konstantin Lübeck <konstantin.luebeck@uni-tuebingen.de>
2023-06-06 15:41:02 +0200

    Added minimal.lua for debugging installation with packer.

---
+++ minimal.lua
@@ -0,0 +1,41 @@
+-- ignore default config and plugins
+vim.opt.runtimepath:remove(vim.fn.expand('~/.config/nvim'))
+vim.opt.packpath:remove(vim.fn.expand('~/.local/share/nvim/site'))
+
+-- append test directory
+local test_dir = '/tmp/nvim-config'
+vim.opt.runtimepath:append(vim.fn.expand(test_dir))
+vim.opt.packpath:append(vim.fn.expand(test_dir))
+
+-- install packer
+local install_path = test_dir .. '/pack/packer/start/packer.nvim'
+local install_plugins = false
+
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+    vim.cmd('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
+    vim.cmd('packadd packer.nvim')
+    install_plugins = true
+end
+
+local packer = require('packer')
+
+packer.init({
+    package_root = test_dir .. '/pack',
+    compile_path = test_dir .. '/plugin/packer_compiled.lua'
+})
+
+packer.startup(function(use)
+    -- Packer can manage itself
+    use('wbthomason/packer.nvim')
+
+    use({'/local/luebeck/Programming/pynvimExample', run=":UpdateRemotePlugins"})
+
+    if install_plugins then
+        packer.sync()
+    end
+end)
+
+require('pynvimExample').setup({
+    height = 20
+})
+
