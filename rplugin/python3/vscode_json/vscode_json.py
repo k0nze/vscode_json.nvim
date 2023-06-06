38522a86d4bad0804f2a6a324530c152678493af
Konstantin Lübeck <konstantin.luebeck@uni-tuebingen.de>
2023-06-06 16:41:40 +0200

    Added check if .vscode dir exists and test files in test directory.

--- rplugin/python3/vscode_json/vscode_json.py
+++ rplugin/python3/vscode_json/vscode_json.py
@@ -1,4 +1,5 @@
 import pynvim
+import os
 
 from pynvim import Nvim
 
@@ -7,7 +8,18 @@ from pynvim import Nvim
 class VSCodeJSON:
     def __init__(self, nvim: Nvim) -> None:
         self.nvim = nvim
+        self.vscode_dir_path = None
 
-    @pynvim.command("VSCodeJSONRun", nargs=0)
+        self.check_dir_exists(None)
+
+    @pynvim.function("VSCodeJSONCheckDirExists")
+    def check_dir_exists(self, _) -> None:
+        current_dir_path = self.nvim.command_output("pwd")
+        vscode_dir_path = str(current_dir_path) + "/.vscode"
+
+        if os.path.isdir(vscode_dir_path):
+            self.vscode_dir_path = vscode_dir_path
+
+    @pynvim.command("VSCodeJSONRun", nargs="0")
     def run(self, _):
-        self.nvim.current.line = "Hello"
+        self.nvim.current.line = f"cwd: {self.vscode_dir_path}"
