d6706ff1e379536a8f172ab24a2ea0903983bd5f
Konstantin Lübeck <konstantin.luebeck@uni-tuebingen.de>
2023-06-06 16:58:05 +0200

    Added check if .vscode/launch.json exists and launch.json example for Python.

--- rplugin/python3/vscode_json/vscode_json.py
+++ rplugin/python3/vscode_json/vscode_json.py
@@ -9,17 +9,35 @@ class VSCodeJSON:
     def __init__(self, nvim: Nvim) -> None:
         self.nvim = nvim
         self.vscode_dir_path = None
+        self.vscode_dir_exists()
 
-        self.check_dir_exists(None)
+        self.launch_json_path = None
 
     @pynvim.function("VSCodeJSONCheckDirExists")
-    def check_dir_exists(self, _) -> None:
+    def vscode_dir_exists(self, _=None) -> bool:
         current_dir_path = self.nvim.command_output("pwd")
         vscode_dir_path = str(current_dir_path) + "/.vscode"
 
         if os.path.isdir(vscode_dir_path):
             self.vscode_dir_path = vscode_dir_path
+            return True
+
+        return False
+
+    @pynvim.function("VSCodeJSONLaunchExists")
+    def launch_json_exists(self, _=None) -> bool:
+        if not self.vscode_dir_exists():
+            return False
+
+        launch_json_path = str(self.vscode_dir_path) + "/launch.json"
+
+        if os.path.isfile(launch_json_path):
+            self.launch_json_path = launch_json_path
+            return True
+
+        return False
 
     @pynvim.command("VSCodeJSONRun", nargs="0")
     def run(self, _):
-        self.nvim.current.line = f"cwd: {self.vscode_dir_path}"
+        self.launch_json_exists()
+        self.nvim.current.line = f"cwd: {self.launch_json_path}"
